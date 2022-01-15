7f2f1733bf94a9f3db8ba4f1d567c8ef5a9c280c
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2022-01-15 18:04:32 +0100

    William % R indicator

---
+++ src/main/scala/mf/dabi/pso/techIndicatorTradingSystem/finance/indicators/WLLR.scala
@@ -0,0 +1,30 @@
+package mf.dabi.pso.techIndicatorTradingSystem.finance.indicators
+
+import org.apache.spark.sql.functions.{last, lit, max, min}
+import org.apache.spark.sql.{Column, DataFrame}
+
+/** Info.
+ * https://www.investopedia.com/terms/w/williamsr.asp
+ * https://www.metatrader5.com/es/terminal/help/indicators/oscillators/wpr
+ * */
+sealed case class GWLLR(period: Int) extends Indicator {
+  final val name: String = "WllR"
+  final val ref: String = s"$name$period".toLowerCase
+
+  def calculate(df: DataFrame): DataFrame = {
+    val intervalObj: Interval = interval(df, period)
+    val (dfIndicator, dfAux, intervalDF): (DataFrame, DataFrame, DataFrame) = intervalObj.deploy()
+    val wllrCol: Column = wllr
+    intervalDF.select(dfIndicator(id), dfAux(closeAux)).groupBy(dfIndicator(id)).agg(wllrCol)
+  }
+
+  def wllr: Column = {
+    val lowestPrice: Column = min(closeAux)
+    val highestPrice: Column = max(closeAux)
+    val lastClose: Column = last(closeAux)
+    (lit(-100) * ((highestPrice - lastClose) / (highestPrice - lowestPrice))).as(ref)
+  }
+}
+
+object WLLR extends GWLLR(14)
+
