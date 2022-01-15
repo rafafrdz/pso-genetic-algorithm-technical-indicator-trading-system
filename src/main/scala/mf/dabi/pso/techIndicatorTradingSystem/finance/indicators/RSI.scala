b0af61b1006620a7ccf00be8767dd08429e7e3ff
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2022-01-15 13:16:26 +0100

    indicador RSI

---
+++ src/main/scala/mf/dabi/pso/techIndicatorTradingSystem/finance/indicators/RSI.scala
@@ -0,0 +1,40 @@
+package mf.dabi.pso.techIndicatorTradingSystem.finance.indicators
+
+import org.apache.spark.sql.functions.{lit, negate, when}
+import org.apache.spark.sql.{Column, DataFrame}
+
+
+sealed case class GRSI(period: Int) extends Indicator {
+  protected final val name: String = "RSI"
+  protected final val ref: String = s"$name$period".toLowerCase
+  private final val id: String = "id"
+  private final val idAux: String = s"${id}_aux"
+  private final val close: String = "close"
+  private final val closeAux: String = "closeAux"
+  private final val (u, d): (String, String) = ("u", "d")
+
+  def calculate(df: DataFrame): DataFrame = {
+    val dfIndicator: DataFrame = df.select(id, close)
+    val dfAux: DataFrame = dfIndicator.withColumn(idAux, dfIndicator(id)).withColumn(closeAux, dfIndicator(close))
+    val joinExpr: Column = (dfIndicator(id) - dfAux(idAux)) === 1
+
+    /** Calcular RS */
+    val diffClose: Column = dfIndicator(close) - dfAux(closeAux)
+    val cond: Column = diffClose.gt(0)
+    val colU: Column = when(cond, diffClose).otherwise(lit(0)).as(u)
+    val colD: Column = when(cond, lit(0)).otherwise(negate(diffClose)).as(d)
+
+    val join: DataFrame = dfIndicator.join(dfAux, joinExpr).select(dfIndicator(id), colU, colD)
+    val emaObj: EMA = EMA(period)
+    val rsiCol: Column = rsi(rs(join, emaObj))
+    join.select(dfIndicator(id), rsiCol).where(dfIndicator(id).geq(period))
+  }
+
+  def rs(df: DataFrame, emaObj: EMA): Column = emaObj.ema(df, u) / emaObj.ema(df, d)
+
+  def rsi(rs: Column): Column = (lit(100) - (lit(100) / (lit(1) + rs))).as(ref)
+}
+
+object RSI extends GRSI(14)
+
+
