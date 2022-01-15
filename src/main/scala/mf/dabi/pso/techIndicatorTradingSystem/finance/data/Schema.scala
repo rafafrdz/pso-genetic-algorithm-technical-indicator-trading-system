4e46136d8a4e77c1908a083bf0865a6dfc2913f2
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2022-01-15 13:15:21 +0100

    Ajuste de precision DecimalType

--- src/main/scala/mf/dabi/pso/techIndicatorTradingSystem/finance/data/Schema.scala
+++ src/main/scala/mf/dabi/pso/techIndicatorTradingSystem/finance/data/Schema.scala
@@ -1,7 +1,7 @@
 package mf.dabi.pso.techIndicatorTradingSystem.finance.data
 
 import org.apache.spark.sql.Encoders
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{DecimalType, StructField, StructType}
 
 import scala.reflect.runtime.universe.TypeTag
 
@@ -13,7 +13,15 @@ object Schema {
   type Decimal = java.math.BigDecimal
   type Timestamp = java.sql.Timestamp
 
-  def getFrom[T <: Product : TypeTag]: StructType = Encoders.product[T].schema
+  def getFrom[T <: Product : TypeTag]: StructType = adjust(Encoders.product[T].schema)
+
+  private def adjust(schema: StructType): StructType = {
+    val schAdj: Seq[StructField] = schema.map {
+      case StructField(name, _: DecimalType, nullable, metadata) => StructField(name, DecimalType(20, 6), nullable, metadata)
+      case field => field
+    }
+    StructType(schAdj)
+  }
 
 
 }
