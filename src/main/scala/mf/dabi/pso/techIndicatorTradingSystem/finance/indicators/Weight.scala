44833305d544909a66f6498153a9994f8053dbda
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2022-01-18 09:15:02 +0100

    edit weight

--- src/main/scala/mf/dabi/pso/techIndicatorTradingSystem/finance/indicators/Weight.scala
+++ src/main/scala/mf/dabi/pso/techIndicatorTradingSystem/finance/indicators/Weight.scala
@@ -12,8 +12,13 @@ trait Weight extends Serializable {
 
   lazy val refWeight: String = s"${ref}_weight"
   lazy val refWeightRaw: String = s"${refWeight}_raw"
-  lazy val weightRaw: Column = udfRndm(lit(1)).cast(DecimalType(20, 6)).as(refWeightRaw)
+  /** Version Random a nivel de fila (para cada dia) */
+//  lazy val weightRaw: Column = udfRndm(lit(1)).cast(DecimalType(20, 6)).as(refWeightRaw)
+
+  /** Version Random a nivel de columna */
+  lazy val weightRaw: Column = lit(Random.nextInt(10).asInstanceOf[Double]).cast(DecimalType(20, 6)).as(refWeightRaw)
   lazy val weight: Column => Column = (sumWeight: Column) => (col(refWeightRaw) / sumWeight).cast(DecimalType(20, 6)).as(refWeight)
+  def customWeight(w: Double): Column = lit(w).cast(DecimalType(20, 6)).as(refWeightRaw)
 
   val lambdardnm: Int => Option[Double] = (_: Int) => Try(Random.nextInt(10).asInstanceOf[Double]).toOption
 
